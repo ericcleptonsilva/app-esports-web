2f45d6418130c6daef0f12eeb56e1b4687601886
Eric Cleptonsilva <ericcleptonsilva@gmail.com>
2022-09-18 02:06:42 -0300

    estilizando background, o texto principal e adicionando as imagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import './styles.css'
+import './styles/main.css'
+import logoImg from './assets/Logo.svg'
 
 function App() {
-    return(<h1>Hello Word</h1>)
+    return(
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+        <img src={logoImg} alt=''/>
+        <h1 className="text-6xl text-white font-black mt-20">
+            Seu <span className=" bg-nlw-gradient bg-clip-text text-transparent" >duo</span> está aqui.
+        </h1>
+
+        <div className="grid grid-cols-6 gap-6 mt-16">
+            <a href=""><img src="./assets/games-1.jpg" alt="Fortnite"/></a>
+        </div>
+    </div>)
 }
 
 export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.tsx","./index.html"],
   theme: {
-    extend: {},
+    extend: {
+
+      backgroundImage:{
+       backgroundGalaxy: "url('/background.svg')",
+       'nlw-gradient':'linear-gradient(89.86deg, #9572FC 20.08%, #43E7AD 36.94%, #E1D55D 44.57%);'
+
+      }
+    },
   },
   plugins: [],
 }
